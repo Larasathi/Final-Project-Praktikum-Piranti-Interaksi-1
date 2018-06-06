1dcff79638182cdf59c23fa68ec47ae7d1c0d311
Mohammad Yusuf Prastomo <myprasto@gmail.com>
2018-06-06 11:38:55 +0700

    3rd commit

---
+++ Assets/TargetMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour {
+
+    public float xPos;
+    public float yVel;
+    public float xVel;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        xPos = Random.Range(-8, 8);
+        yVel = Random.Range(9, 13);
+        xVel = Random.Range(-3, 3);
+
+        if (this.transform.position.y < -3)
+        {
+            this.transform.position = new Vector2(xPos, -3);
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(xVel, yVel);
+        }
+	}
+    
+
+
+    public void OnMouseDown()
+    {
+        Debug.Log("clicked");
+        Destroy(this.gameObject);
+        //Instantiate(this.gameObject);
+    }
+}
